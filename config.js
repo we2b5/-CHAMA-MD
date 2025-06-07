37dbc314c1d94135a9d959ff3cd89965bfc1d5ab
we2b5 <webg721@gmail.com>
2025-06-07 11:18:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "CHAMA-MD=EjsUBAjL#S-8qiLbh-ywqQyiF_BHiw9eVK5nDNiHvG4Efir3FZjc",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY CHAMA-MD 🤍*",
 // set the auto reply massage on status reply  
